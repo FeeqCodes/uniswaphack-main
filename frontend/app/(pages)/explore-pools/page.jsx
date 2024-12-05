ab662e44727544c57a555bd109719f4a7675a721
Moh <Mohofficial11@outlook.com>
2024-12-05 20:02:22 +0100

    add explore page

---
+++ frontend/app/(pages)/explore-pools/page.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import ExploreSection from "@/app/components/Sections/Explore/ExploreSection";
+
+
+
+const ExplorePage = () => {
+  return (
+    <>
+    <ExploreSection />
+    </>
+  )
+};
+
+export default ExplorePage;
