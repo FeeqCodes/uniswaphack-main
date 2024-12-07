dabdb99df5d2e384bf48393b5e5ce27f2867cee6
Moh <Mohofficial11@outlook.com>
2024-12-07 13:44:24 +0100

    update deployment script

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -4,22 +4,14 @@ pragma solidity ^0.8.24;
 import {Script} from "forge-std/Script.sol";
 import {Main} from "../src/main.sol";
 
-
-
-
 contract DeployScript is Script {
-    // Define Uniswap V4 addresses for different networks
-    address constant SEPOLIA_V4_POOL_MANAGER = 0x64255ed21366DB43d89736EE48928b890A84E2Cb;
-    address constant GOERLI_V4_POOL_MANAGER = 0x3A9D48AB9751398BbFa63ad67599Bb04e4BdF98b;
-    
-
+    address constant SEPOLIA_V4_POOL_MANAGER = 0x8c4bcbe6b9ef47855f97e675296fa3f6fafa5f1a;
+    address constant SEPOLIA_V4_POSITION_MANAGER = 0x1b1c77b606d13b09c84d1c7394b96b147bc03147;
 
     function run() external returns (Main) {
         vm.startBroadcast();
-        // Use the appropriate address based on your target network
-        Main main = new Main(SEPOLIA_V4_POOL_MANAGER, payable(SEPOLIA_V4_POOL_MANAGER));
+        Main main = new Main(SEPOLIA_V4_POOL_MANAGER, payable(SEPOLIA_V4_POSITION_MANAGER));
         vm.stopBroadcast();
-
         return main;
     }
 }
